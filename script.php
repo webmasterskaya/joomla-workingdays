2d937623b93af3b402b70262f96009d3ce219405
Artem Vasilev <kern.usr@gmail.com>
2022-02-01 11:52:52 +0300

    LICENSE

---
+++ script.php
@@ -0,0 +1,190 @@
+<?php
+/**
+ * @package    workingdays
+ *
+ * @author     Артём <your@email.com>
+ * @copyright  A copyright
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * @link       http://your.url.com
+ */
+
+use Joomla\CMS\Factory;
+use Joomla\CMS\Installer\Adapter\PackageAdapter;
+use Joomla\CMS\Installer\InstallerAdapter;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\Version;
+
+defined('_JEXEC') or die;
+
+/**
+ * Workingdays script file.
+ *
+ * @package   workingdays
+ * @since     1.0.0
+ */
+class plgSystemWorkingdaysInstallerScript
+{
+	/**
+	 * Minimum PHP version required to install the extension.
+	 *
+	 * @var  string
+	 *
+	 * @since  __DEPLOY_VERSION__
+	 */
+	protected $minimumPhp = '7.1';
+
+	/**
+	 * Minimum Joomla version required to install the extension.
+	 *
+	 * @var  string
+	 *
+	 * @since  __DEPLOY_VERSION__
+	 */
+	protected $minimumJoomla = '3.9.0';
+
+
+	/**
+	 * Runs right before any installation action.
+	 *
+	 * @param   string                           $type    Type of PostFlight action.
+	 * @param   InstallerAdapter|PackageAdapter  $parent  Parent object calling object.
+	 *
+	 * @throws  Exception
+	 *
+	 * @return  boolean True on success, false on failure.
+	 *
+	 * @since  __DEPLOY_VERSION__
+	 */
+	function preflight($type, $parent)
+	{
+		// Check compatible
+		if (!$this->checkCompatible('PLG_SYSTEM_WORKINGDAYS_'))
+		{
+			return false;
+		}
+
+		return true;
+	}
+
+	/**
+	 * Method to check compatible.
+	 *
+	 * @param   string  $prefix  Language constants prefix.
+	 *
+	 * @throws  Exception
+	 *
+	 * @return  boolean True on success, false on failure.
+	 *
+	 * @since  __DEPLOY_VERSION__
+	 */
+	protected function checkCompatible($prefix = null)
+	{
+		// Check old Joomla
+		if (!class_exists('Joomla\CMS\Version'))
+		{
+			JFactory::getApplication()->enqueueMessage(
+				JText::sprintf(
+					$prefix . 'ERROR_COMPATIBLE_JOOMLA',
+					$this->minimumJoomla
+				), 'error'
+			);
+
+			return false;
+		}
+
+		$app = Factory::getApplication();
+
+		// Check PHP
+		if (!(version_compare(PHP_VERSION, $this->minimumPhp) >= 0))
+		{
+			$app->enqueueMessage(
+				Text::sprintf(
+					$prefix . 'ERROR_COMPATIBLE_PHP', $this->minimumPhp
+				),
+				'error'
+			);
+
+			return false;
+		}
+
+		// Check joomla version
+		if (!(new Version())->isCompatible($this->minimumJoomla))
+		{
+			$app->enqueueMessage(
+				Text::sprintf(
+					$prefix . 'ERROR_COMPATIBLE_JOOMLA', $this->minimumJoomla
+				),
+				'error'
+			);
+
+			return false;
+		}
+
+		return true;
+	}
+
+	/**
+	 * Runs right after any installation action.
+	 *
+	 * @param   string            $type    Type of PostFlight action. Possible values are:
+	 * @param   InstallerAdapter  $parent  Parent object calling object.
+	 *
+	 * @throws  Exception
+	 *
+	 * @return  boolean True on success, false on failure.
+	 *
+	 * @since  __DEPLOY_VERSION__
+	 */
+	function postflight($type, $parent)
+	{
+		// Enable plugin
+		if ($type == 'install')
+		{
+			$this->enablePlugin($parent);
+		}
+
+		// Refresh media
+		if ($type === 'update')
+		{
+			(new Version())->refreshMediaVersion();
+		}
+
+		return true;
+	}
+
+	/**
+	 * Enable plugin after installation.
+	 *
+	 * @param   InstallerAdapter  $parent  Parent object calling object.
+	 *
+	 * @since  __DEPLOY_VERSION__
+	 */
+	protected function enablePlugin($parent)
+	{
+		// Prepare plugin object
+		$plugin          = new stdClass();
+		$plugin->type    = 'plugin';
+		$plugin->element = $parent->getElement();
+		$plugin->folder  = (string) $parent->getParent()->manifest->attributes(
+		)['group'];
+		$plugin->enabled = 1;
+
+		// Update record
+		Factory::getDbo()->updateObject(
+			'#__extensions', $plugin, array('type', 'element', 'folder')
+		);
+	}
+
+	/**
+	 * This method is called after extension is uninstalled.
+	 *
+	 * @param   InstallerAdapter  $parent  Parent object calling object.
+	 *
+	 * @since  __DEPLOY_VERSION__
+	 */
+	public function uninstall($parent)
+	{
+	}
+
+
+}
---
+++ workingdays.php
@@ -0,0 +1,110 @@
+<?php
+/**
+ * @package    workingdays
+ *
+ * @author     Артём <your@email.com>
+ * @copyright  A copyright
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * @link       http://your.url.com
+ */
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Application\CMSApplication;
+use Joomla\CMS\Date\Date;
+use Joomla\CMS\Factory;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\Plugin\CMSPlugin;
+use Webmasterskaya\ProductionCalendar\Calendar as Calendar;
+
+/**
+ * Workingdays plugin.
+ *
+ * @package   workingdays
+ * @since     1.0.0
+ */
+class plgSystemWorkingdays extends CMSPlugin
+{
+	/**
+	 * Application object
+	 *
+	 * @var    CMSApplication
+	 * @since  1.0.0
+	 */
+	protected $app;
+
+	/**
+	 * Database object
+	 *
+	 * @var    JDatabaseDriver
+	 * @since  1.0.0
+	 */
+	protected $db;
+
+	/**
+	 * Affects constructor behavior. If true, language files will be loaded automatically.
+	 *
+	 * @var    boolean
+	 * @since  1.0.0
+	 */
+	protected $autoloadLanguage = true;
+
+	/**
+	 * onAfterCompileHead.
+	 *
+	 * @return  void
+	 *
+	 * @since   1.0
+	 */
+	public function onBeforeCompileHead()
+	{
+		JLoader::registerNamespace(
+			'Webmasterskaya\\ProductionCalendar',
+			JPATH_LIBRARIES . '/lib_production_calendar/ProductionCalendar',
+			false,
+			false,
+			'psr4'
+		);
+
+		if (!class_exists('Webmasterskaya\\ProductionCalendar\\Calendar'))
+		{
+			return;
+		}
+
+		if (!Factory::getApplication()->isClient('site'))
+		{
+			return;
+		}
+
+		$today = new Date('TODAY');
+
+		if (Calendar::isPreHoliday($today))
+		{
+			$message = Text::_(
+				$this->params->get(
+					'pre_holiday_message',
+					'PLG_SYSTEM_WORKINGDAYS_PARAMS_PRE_HOLIDAY_MESSAGE'
+				)
+			);
+		}
+
+		if (!Calendar::isWorking($today))
+		{
+			$message = Text::sprintf(
+				$this->params->get(
+					'not_working_day_message',
+					'PLG_SYSTEM_WORKINGDAYS_PARAMS_NOT_WORKING_DAY_MESSAGE'
+				),
+				Calendar::find($today)->working()->format(
+					Text::_('DATE_FORMAT_LC1')
+				)
+			);
+		}
+
+		if (isset($message))
+		{
+			$document = Factory::getDocument();
+			$document->addScriptOptions('workingdays', ['message' => $message]);
+		}
+	}
+}
